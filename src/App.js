bb014972bc86370f5afcacfe7f368a80b7b919f1
Pragna235 <yanduripragna2003@gmail.com>
2024-02-12 17:37:00 +0530

    React Weather App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { WeatherApp } from './Components/WeatherApp/WeatherApp';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp/>
+    </div>
+  );
+}
+
+export default App;
